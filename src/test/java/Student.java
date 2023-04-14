f1ece36a35975abaf732eeff23f374210e4afccc
Minh Nguyen <quangminh.nguyen8989@gmail.com>
2023-04-14 14:25:26 -0500

    update master branch

--- src/test/java/Student.java
+++ src/test/java/Student.java
@@ -1,45 +1,6 @@
 import java.util.ArrayList;
 
 public class Student {
-    String name;
-    long id;
 
-    ArrayList<Integer> gradeLists;
 
-    public Student(String name, int id) {
-        this.name = name;
-        this.id = id;
-    }
-
-    // returns the student's id
-    public long getId(){
-        return id;
-    }
-
-    // returns the student's name
-    public String getName(){
-        return name;
-    }
-
-    // adds the given grade to the grades list
-    public void addGrade(int grade){
-        gradeLists.add(90);
-        gradeLists.add(89);
-        gradeLists.add(99);
-    }
-
-    // returns the list of grades
-    public ArrayList<Integer> getGrades(){
-        return gradeLists;
-    }
-
-    // returns the average of the students grades
-    public double getGradeAverage(){
-        double sum = 0;
-        for (int grade : gradeLists) {
-            sum += grade;
-        }
-        double average = sum / gradeLists.size();
-        return average;
-    }
 }
